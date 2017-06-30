fd9ed8c0e3707be870d256cee7174f0f41e49797
bmintz <bmintz@users.noreply.github.com>
2017-06-29 21:01:01 -0500

    server.py: initial file commit

---
+++ multicraft-backup/server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+server: utilities to start, and stop the server
+"""
+
+import selenium
+
+
+class ServerBase:
+	def __init__(self, config):
+		...
