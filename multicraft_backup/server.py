0a87e3c2408fbf9dec8b0d416dcf7e69e479e0c7
bmintz <bmintz@users.noreply.github.com>
2017-06-30 15:13:22 -0500

    server.py: flesh out the class a bit

--- multicraft_backup/server.py
+++ multicraft_backup/server.py
@@ -10,4 +10,19 @@ import selenium
 
 class ServerBase:
 	def __init__(self, config):
-		...
+		self.browser = selenium.webdriver.Chrome()
+		
+		# actions should timeout after 3 seconds
+		self.implicitly_wait(3)
+		self.set_script_timeout(3)
+		
+		self._login()
+	
+	
+	def __getattribute__(self, attribute):
+		"""make all self.browser attributes appear to be self attributes"""
+		
+		try:
+			return object.__getattribute__(self, attribute)
+		except AttributeError:
+			return self.browser.__getattribute__(attribute)
